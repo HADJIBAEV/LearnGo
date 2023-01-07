37b3facdfc0cfede069c7dec8d9a7838ef7d44eb
Hadjibaev Kahramon <hp@oilnew.ru>
2023-01-07 19:42:56 +0300

    Add If else & switch example

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+func main() {
+	// _____ Golang If else example _____
+	/*	var number = [...]int{1, 2, 3, 4}
+		numbers := [...]int{1, 2, 3, 4}
+		if number == numbers {
+			fmt.Println("Very good")
+		} else {
+			fmt.Println(number)
+			fmt.Println(numbers)
+		}
+		a := 12
+		if a > 0 {
+			fmt.Println("a > 0")
+		} else if a != 12 {
+			fmt.Println("a != 12")
+		} else {
+			fmt.Println("a = 12")
+		}
+	*/
+	// _____ Golang Switch example _____
+	/*	a := 12
+		switch a {
+		case 9, 10, 11, 12:
+			fmt.Println("a = 9")
+		case 8:
+			fmt.Println("Case a = 8")
+		case 7:
+			fmt.Println("a = 7")
+		default:
+			fmt.Println("Default variable case")
+		}
+	*/
+
+}
